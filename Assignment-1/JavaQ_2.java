a8461fafe69d462d511ff1130bced3f43a5c4d16
Anish43 <70018561+Anish43@users.noreply.github.com>
2020-08-21 22:33:44 +0530

    Update JavaQ_2.java

--- Assignment-1/JavaQ_2.java
+++ Assignment-1/JavaQ_2.java
@@ -1,54 +1,67 @@
-import java.util.*;
+package com.company;
 
-public class editing_q2 
+import java.util.*;
+public class Main
 {
 
-	public static void main(String[] args) 
-	{
-		Scanner input=new Scanner(System.in);
-		System.out.println("Enter paragraph: ");
-		String para=input.nextLine();
-		System.out.println("Enter size of vector: ");
-		int vecSize=input.nextInt();
-		HashSet<String> vec=new HashSet<String>();
-		System.out.println("Enter "+vecSize+" Strings:");
-		for(int i=0;i<vecSize;i++)
-		{
-			String s=input.next();
-			vec.add(s);
-		}
-		input.close();
-		int n=para.length();
-		int j=0;
-		System.out.println("Paragraph after formatting:");
-		String ours="";
-		while(j<n)
-		{
-			String s="";
-			while(j<n && para.charAt(j)!=' ')
-			{
-				s+=para.charAt(j);
-				j++;
-			}
-			if(vec.contains(s))
-			{
-				ours+=s.charAt(0);
-				for(int i=1;i<s.length();i++)
-				{
-					ours+='*';
-				}
-			}
-			else
-			{
-				ours+=s;
-			}
-			if(j<n && para.charAt(j)==' ')
-			{
-				ours+=" ";
-			}
-			j++;
-		}
-		System.out.println(ours);
-	}
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+ 
 
+                System.out.println("Enter a string:-");
+                String na = sc.nextLine();
+                int n1 = na.length();                // n1 string length
+                String arr[]=na.split("\\ ");
+                int n2= arr.length;               // n2 split array length
+                // System.out.println(n2);
+                Vector v = new Vector();
+                int ch =0;
+                int br=0;
+                int dx=0;
+
+                System.out.println("Enter total number of strings you want to change:-");
+                ch=sc.nextInt();
+                sc.nextLine();
+                for(int i =1;i<=ch;i++)
+                {
+                    System.out.println("Enter "+ i+" string:-");
+                    String nb = sc.nextLine();
+                    v.add(nb);
+                }
+                int x=0;
+                for(int k=0;k<n2;k++)
+                {
+                    if(v.contains(arr[k]))
+                    {
+                        x++;
+                        char[] brr = arr[k].toCharArray();
+                        int n3= brr.length;
+                        //System.out.println(n3);
+                        char[] a = new  char[n3];
+                        for(int i=0;i<n3;i++)
+                        {
+                            if(i==0)
+                            {
+                                a[i]=brr[0];
+                            }
+                            else
+                            {
+                                a[i]='*';
+                            }
+
+                        }
+                        System.out.print(a);
+                    }
+                    else
+                    {
+
+                        System.out.print(arr[k]);
+                        // break;
+                    }
+                    System.out.println(" ");
+                }
+//                break;
+//        }
+    }
 }
