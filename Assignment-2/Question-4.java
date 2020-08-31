29d51aef52c1d9915a2265a2b23e3e3c9dc141da
Anish43 <70018561+Anish43@users.noreply.github.com>
2020-08-31 20:00:12 +0530

    Update Question-4.java

--- Assignment-2/Question-4.java
+++ Assignment-2/Question-4.java
@@ -1,15 +1,12 @@
+package com.company;
 public class Main {
-
     public static void main(String[] args) {
         int sum=0, i=1;
-        for(; i<Integer.MAX_VALUE && sum<Integer.MAX_VALUE; i++) {
-            if(sum+i>Integer.MAX_VALUE){
-              break;
-            }
+        for(; i<Integer.MAX_VALUE; i++) {
+            if(sum >= Integer.MAX_VALUE-i) break;
             sum+=i;
-            if(sum==i*i)
+            if((int)Math.sqrt(sum)==i)
                 System.out.println(i);
-                
         }
         System.out.println("The program ended at "+i);
     }
